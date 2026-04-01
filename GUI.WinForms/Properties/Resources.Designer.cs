583eece7254cab9fb9d355393ba5ede57f518cf5
Luca Veronelli <l.veronelli@stem.it>
2026-04-01 12:53:31 +0200

    Cambia da GUI.Windows a GUI.WinForms

--- GUI.WinForms/Properties/Resources.Designer.cs
+++ GUI.WinForms/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace GUI.Windows.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI.Windows.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI.WinForms.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
